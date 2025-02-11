7e02ab9bd45e01744152bc735a34ae49b68d88d1
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-11 15:16:01 +0100

    Init: project

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'site_name' => 'Bucket List',
+            'user' => 'Admin'
+        ]);
+    }
+
+
+    public function about(): Response
+    {
+        
+    }
+}
