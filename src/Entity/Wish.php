fdd0322f896812ff5087c5a21aef437383c0db9c
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-14 09:48:16 +0100

    Create: entity Wish

---
+++ src/Entity/Wish.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WishRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WishRepository::class)]
+class Wish
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 250)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $author = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isPublished = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateCreatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getDateCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->dateCreatedAt;
+    }
+
+    public function setDateCreatedAt(?\DateTimeImmutable $dateCreatedAt): static
+    {
+        $this->dateCreatedAt = $dateCreatedAt;
+
+        return $this;
+    }
+}
