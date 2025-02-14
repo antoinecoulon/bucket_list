acbec3169280a5e00873b9c04e86cdcf4b6e5aec
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-14 09:48:29 +0100

    Add: wishes list and detail

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,14 +12,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WishController extends AbstractController
 {
     #[Route('/', name: 'list', methods: ['GET'])]
-    public function list(): Response
+    public function list(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('wish/list.html.twig', []);
+        $wishRepository = $entityManager->getRepository(Wish::class);
+        $wishes = $wishRepository->findAllOrderedByDate();
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id'=>'\d+'], methods: ['GET'])]
-    public function detail($id): Response
+    public function detail($id, Wish $wish): Response
     {
-        return $this->render('wish/detail.html.twig', []);
+        return $this->render('wish/detail.html.twig', [
+            'wish' => $wish
+        ]);
     }
 }
