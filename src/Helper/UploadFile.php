0b0b20ce517effd5281c89e3fa7bc8c7641de3a6
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-21 10:25:14 +0100

    Create UploadFile.php

---
+++ src/Helper/UploadFile.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helper;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class UploadFile
+{
+
+    public function __construct(private readonly SluggerInterface $slugger){}
+    public function upload(UploadedFile $file, String $fileName, String $dest): String
+    {
+        $name = $this->slugger->slug($fileName) . '-' . uniqid('', false) . '.' . $file->guessExtension();
+        $file->move($dest, $name);
+        return $name;
+    }
+
+}
